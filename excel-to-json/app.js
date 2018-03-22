a6e878cb0c86348cdbe5f59955a32ec85da7b997
jETA <jeta@jetalog.net>
2018-03-22 22:50:38 +0900

    excel-to-json 예제 생성

---
+++ excel-to-json/app.js
@@ -0,0 +1,55 @@
+const express = require('express');
+const http = require('http');
+const bodyParser = require('body-parser');
+const multiparty = require('multiparty');
+const xlsx = require('xlsx');
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    limit: '150mb',
+    extended: false,
+}));
+
+app.get('/', (req, res, next) => {
+    let contents = '';
+    contents += '<html><body>';
+    contents += '   <form action="/" method="POST" enctype="multipart/form-data">';
+    contents += '       <input type="file" name="xlsx" />';
+    contents += '       <input type="submit" />';
+    contents += '   </form>';
+    contents += '</body></html>';
+
+    res.send(contents);
+});
+
+app.post('/', (req, res, next) => {
+    const resData = {};
+
+    const form = new multiparty.Form({
+        autoFiles: true,
+    });
+
+    form.on('file', function (name, file) {
+        const workbook = xlsx.readFile(file.path);
+        const sheetnames = Object.keys(workbook.Sheets);
+
+        let i = sheetnames.length;
+
+        while (i--) {
+            const sheetname = sheetnames[i];
+            resData[sheetname] = xlsx.utils.sheet_to_json(workbook.Sheets[sheetname]);
+        }
+    });
+
+    form.on('close', () => {
+        res.send(resData);
+    });
+
+    form.parse(req);
+});
+
+http.createServer(app).listen(3000, () => {
+    console.log('HTTP server listening on port ' + 3000);
+});
